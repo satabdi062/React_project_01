beb8ef6813f086afb0ec8789175b751a6b4563b2
satabdi062 <satabdinaskar062@gmail.com>
2024-07-06 15:26:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import { CORE_CONCEPTS } from './data.js';
+
+function App() {
+  
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2> 
+          <ul>
+            <CoreConcepts{...CORE_CONCEPTS[0]}/>
+            <CoreConcepts{...CORE_CONCEPTS[1]}/>
+            <CoreConcepts{...CORE_CONCEPTS[2]}/>
+            <CoreConcepts{...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
